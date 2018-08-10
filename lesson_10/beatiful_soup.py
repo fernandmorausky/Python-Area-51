7dfb9554280d06fe044d068412fd1d16722f19a8
Jean Pierre Mandujano G <jeanpierre.mandujano@gmail.com>
2018-08-10 17:07:43 -0500

    pre lesson 10

---
+++ lesson_10/beatiful_soup.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get('https://en.wikipedia.org/wiki/Python_(programming_language)')
+soup = BeautifulSoup(req.text, "lxml")
+
+#print('Tipo: {}\nEstructura: {}'.format(type(soup), dir(soup)))
+
+#print('Título en html: {}\nNombre del título: {}\nContenido del título: {}'.format(soup.title, soup.title.name, soup.title.string))
+
+print(type(soup.h1))
+print(soup.h1)
+print(soup.h1.string)
+print(soup.h1['class'])
+print(soup.h1['id'])
+print(soup.h1.attrs)
+
+
+soup.h1['class'] = 'firstHeading, mainHeading'
+soup.h1.string.replace_with("Python - Programming Language")
+del soup.h1['lang']
+del soup.h1['id']
+
+print(soup.h1)
+
+
+for h2 in soup.find_all('h2'):
+    print(h2.text)
+
+
+print(soup.p.a)
+print(soup.p.find_all('a'))
+
+
+print(soup.p.contents)
+print(soup.p.contents[6])
+print(soup.p.children)
+for child in soup.p.children:
+    print(child.name)
